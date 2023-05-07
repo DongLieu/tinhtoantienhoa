fb80760aa12b55d217b714a03bfbdf08d8d83435
DongLieu <duongduydongtq@gmail.com>
2023-05-07 17:02:56 +0700

    read file

--- code/MOTLBO.py
+++ code/MOTLBO.py
@@ -1,19 +1,5 @@
-import numpy as np
-import os
+import input
 
-# Bộ dữ liệu:
-name_file = "cogent_center_0"
+name_folder = "nsf_urban_0"
+f, l, n, dothi, m, canhdothi = input.read_input(name_folder)
 
-# Lấy đường dẫn tuyệt đối đến thư mục hiện tại
-current_dir = os.path.abspath(os.getcwd())
-
-# Kết hợp đường dẫn tương đối để tạo đường dẫn đầy đủ đến file input.txt
-file_path = os.path.join(current_dir, "dataset", name_file, "input.txt")
-
-# Đọc nội dung của file và chia theo dòng
-with open(file_path, "r") as f:
-    data = f.readlines()
-
-# Hiển thị danh sách các dòng
-for line in data:
-    print(line)

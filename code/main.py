c64ced23fb13d13874c1fe1b6ca3e4a05b8c9bb7
DongCoNY <dong183497@gmail.com>
2023-07-02 17:00:49 +0700

    run

--- code/main.py
+++ code/main.py
@@ -12,11 +12,11 @@ from Solution import *
 miens = ["cogent"]#"nsf"]#, , "conus"]
 vungs = ["center"]#, "rural", "uniform", "urban"]
 
-requests = [10]#, 20, 30]
+requests = [10, 20, 30]
 
 TIMELIMIT = 600
 
-i_s = [2]
+i_s = [2, 1, 0, 3, 4]
 
 for mien in miens:
     for vung in vungs:
@@ -38,29 +38,28 @@ for mien in miens:
                 NUM_REMOVE_MOTLBO = 20
 
                 motlbo = MOTLBO(N_POP_MOTLBO, GEN_MOTLBO, TIMELIMIT, NUM_REMOVE_MOTLBO, sol_mau)
-                motlbo.initialize_population()
-                # motlbo.run()
+                motlbo.run()
 
-                # # ############################-MOEAD-##############################
-                # N_POP_MOEAD = 20
+                # ############################-MOEAD-##############################
+                N_POP_MOEAD = 20
 
-                # GEN_MOEAD = 1000
+                GEN_MOEAD = 1000
 
-                # moead = MOEAD(N_POP_MOEAD, GEN_MOEAD, TIMELIMIT, name_folder, request)
-                # moead.run()
+                moead = MOEAD(N_POP_MOEAD, GEN_MOEAD, TIMELIMIT, name_folder, request)
+                moead.run()
 
-                # # # ############################-NSGA2-##################################
-                # N_POP_NSGA2 = 50
-                # GEN_NSGA2 = 50
-                # NUM_REMOVE_NSGA2 = 10
+                # # ############################-NSGA2-##################################
+                N_POP_NSGA2 = 50
+                GEN_NSGA2 = 50
+                NUM_REMOVE_NSGA2 = 10
 
-                # nsga2 = NSGA2(N_POP_NSGA2, GEN_NSGA2, TIMELIMIT, NUM_REMOVE_NSGA2, name_folder, request)
-                # nsga2.run()
+                nsga2 = NSGA2(N_POP_NSGA2, GEN_NSGA2, TIMELIMIT, NUM_REMOVE_NSGA2, name_folder, request)
+                nsga2.run()
 
-                # # ##########################-ISO-##################################
-                # N_POP_ISO = 100
-                # GEN_ISO = 1000
-                # NUM_REMOVE_ISO = 40
+                # ##########################-ISO-##################################
+                N_POP_ISO = 100
+                GEN_ISO = 1000
+                NUM_REMOVE_ISO = 40
 
-                # iso = ISO(N_POP_ISO, GEN_ISO, TIMELIMIT, NUM_REMOVE_ISO, name_folder, request)
-                # iso.run()
+                iso = ISO(N_POP_ISO, GEN_ISO, TIMELIMIT, NUM_REMOVE_ISO, name_folder, request)
+                iso.run()

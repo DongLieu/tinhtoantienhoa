992ce1eb6c731c7a52b2265c31dcf257d194fbf5
DongCoNy <dong183497@gmail.com>
2023-07-08 13:37:39 +0700

    remove

--- code/main_danhgia.py
+++ code/main_danhgia.py
@@ -21,28 +21,35 @@ sfc_set.create_global_info(network)
 network.create_constraints_and_min_paths(sfc_set)
 
 sol_mau = Solution(network, sfc_set)
-sol_mau.name_folder_output = "./code/danhgia/lua_tham_so/motlbo/"
 ############################-MOTLBO-##############################
+sol_mau.name_folder_output = "./code/danhgia/lua_tham_so/motlbo/"
 N_POP_MOTLBO = 100
 GEN_MOTLBO = 1000
-NUM_REMOVE_MOTLBO = 40
+NUM_REMOVE_MOTLBO = [40, 50]
 RATE_CROSS_MOTLBO = 0.8
 
-motlbo = MOTLBO(N_POP_MOTLBO, GEN_MOTLBO, TIMELIMIT, NUM_REMOVE_MOTLBO, RATE_CROSS_MOTLBO, sol_mau)
-motlbo.run()
+for i in NUM_REMOVE_MOTLBO:
+    motlbo = MOTLBO(N_POP_MOTLBO, GEN_MOTLBO, TIMELIMIT, i, RATE_CROSS_MOTLBO, sol_mau)
+    motlbo.run()
 
 # ############################-MOEAD-##############################
-# N_POP_MOEAD = 20
+# sol_mau.name_folder_output = "./code/danhgia/lua_tham_so/moead/"
+# N_POP_MOEAD = 10
 
 # GEN_MOEAD = 1000
+# K = 3
 
-# moead = MOEAD(N_POP_MOEAD, GEN_MOEAD, TIMELIMIT, sol_mau)
+# moead = MOEAD(N_POP_MOEAD, GEN_MOEAD, TIMELIMIT, K, sol_mau)
 # moead.run()
 
-# # # ############################-NSGA2-##################################
-# N_POP_NSGA2 = 50
-# GEN_NSGA2 = 50
-# NUM_REMOVE_NSGA2 = 10
+# # ############################-NSGA2-##################################
+sol_mau.name_folder_output = "./code/danhgia/lua_tham_so/nsga2/"
+N_POP_NSGA2 = 100
+GEN_NSGA2 = 50
+NUM_REMOVE_NSGA2 = [10, 20, 30, 40, 50]
+RATE_CROSS_NSGA2 = 0.8
+RATE_MUTAION_NSGA2 = 0.1
 
-# nsga2 = NSGA2(N_POP_NSGA2, GEN_NSGA2, TIMELIMIT, NUM_REMOVE_NSGA2, sol_mau)
-# nsga2.run()
+for i in NUM_REMOVE_NSGA2:
+    nsga2 = NSGA2(N_POP_NSGA2, GEN_NSGA2, TIMELIMIT, i, RATE_CROSS_NSGA2, RATE_MUTAION_NSGA2, sol_mau)
+    nsga2.run()
